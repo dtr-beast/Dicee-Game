77ee4fd9ff3b8509dfd45f0a2bf6913fc3376182
dtr-beast <55450440+dtr-beast@users.noreply.github.com>
2021-02-22 14:16:47 +0530

    Completed Dicee Game Boss Challenge

--- ts/index.ts
+++ ts/index.ts
@@ -1,8 +1,17 @@
-function randomDice(): number
-{
+function randomDice(): number {
     return Math.floor(Math.random() * 6) + 1;
 }
 
+let img1 = randomDice();
+let img2 = randomDice();
 // Typing the 'getElementById response using the parenthesis
-(<HTMLImageElement>document.getElementById('img1')).src = `images/dice${randomDice()}.png`;
-(<HTMLImageElement>document.getElementById('img2')).src = `images/dice${randomDice()}.png`;
+(<HTMLImageElement>document.getElementById('img1')).src = `images/dice${img1}.png`;
+(<HTMLImageElement>document.getElementById('img2')).src = `images/dice${img2}.png`;
+
+if (img1 > img2) {
+    document.getElementById('heading').innerHTML = '⛳ Player 1 Wins';
+} else if (img1 < img2) {
+    document.getElementById('heading').innerHTML = 'Player 2 Wins ⛳';
+} else {
+    document.getElementById('heading').innerHTML = 'Draw !';
+}
